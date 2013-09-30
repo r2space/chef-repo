4d4d6321064241a64c67a3be9717f4db714fb4c5
tomxu <cntomxu@gmail.com>
2013-09-30 19:34:49 +0800

    设置ap的nginx的配置文件， 移动init.rb中的ap部分到ap.rb中

--- cookbooks/yukari/recipes/ap.rb
+++ cookbooks/yukari/recipes/ap.rb
@@ -12,11 +12,64 @@
 
 #### nginx ####
 template "nginx.repo" do
-      path "/etc/yum.repos.d/nginx.repo"
-        source "nginx.repo.erb"
+  path "/etc/yum.repos.d/nginx.repo"
+  source "nginx.repo.erb"
 end
 yum_package "nginx" do
-      action :install
+  action :install
+end
+template "nginx.config" do
+  path "/etc/nginx/conf.d/default.conf"
+  source "ap.nginx..erb"
+end
+service "nginx"
+ action [:restart, :enable]
+end
+
+#### varnish ####
+remote_file "/root/varnish-release-3.0-1.el6.noarch.rpm" do
+  source "http://repo.varnish-cache.org/redhat/varnish-3.0/el6/noarch/varnish-release-3.0-1.el6.noarch.rpm"
+end
+
+rpm_package "/root/varnish-release-3.0-1.el6.noarch.rpm" do
+  options "--nosignature" 
+  action :install
+end
+
+yum_package "varnish" do
+  action :install
+end
+service "varnish"
+ action [:start, :enable]
+end
+
+#### nodejs ####
+remote_file "/tmp/node-v0.10.18.tgz" do
+  source "http://nodejs.org/dist/v0.10.18/node-v0.10.18-linux-x64.tar.gz"
+  action :create_if_missing
+end
+execute "node unzip" do
+  cwd "/tmp"
+  command <<-EOF
+  tar -zxvf node-v0.10.18.tgz;
+  EOF
+  not_if { ::File.exists?("/tmp/node-v0.10.18-linux-x64")}
+end
+execute "install node" do
+  cwd "/tmp"
+  command <<-EOF
+  rm -rf /opt/node
+  rm -f /usr/bin/node
+  rm -f /usr/bin/npm
+  cp -R -n  node-v0.10.18-linux-x64 /opt/node
+  ln -s /opt/node/bin/node /usr/bin/node
+  ln -s /opt/node/lib/node_modules/npm/bin/npm-cli.js /usr/bin/npm
+  EOF
+end
+
+#### git ####
+yum_package "git" do
+  action :install
 end
 
 # Install SmartCore
--- cookbooks/yukari/recipes/init.rb
+++ cookbooks/yukari/recipes/init.rb
@@ -9,58 +9,6 @@
 
 include_recipe "yum::epel"
 
-#### varnish ####
-remote_file "/root/varnish-release-3.0-1.el6.noarch.rpm" do
-  source "http://repo.varnish-cache.org/redhat/varnish-3.0/el6/noarch/varnish-release-3.0-1.el6.noarch.rpm"
-end
-
-rpm_package "/root/varnish-release-3.0-1.el6.noarch.rpm" do
-  options "--nosignature"
-  action :install
-end
-
-yum_package "varnish" do
-  action :install
-end
-
-#### nodejs ####
-remote_file "/tmp/node-v0.10.18.tgz" do
-  source "http://nodejs.org/dist/v0.10.18/node-v0.10.18-linux-x64.tar.gz"
-  action :create_if_missing
-end
-execute "node unzip" do
-  cwd "/tmp"
-  command <<-EOF
-  tar -zxvf node-v0.10.18.tgz;
-  EOF
-  not_if { ::File.exists?("/tmp/node-v0.10.18-linux-x64")}
-end
-execute "install node" do
-  cwd "/tmp"
-  command <<-EOF
-  rm -rf /opt/node
-  rm -f /usr/bin/node
-  rm -f /usr/bin/npm
-  cp -R -n  node-v0.10.18-linux-x64 /opt/node
-  ln -s /opt/node/bin/node /usr/bin/node
-  ln -s /opt/node/lib/node_modules/npm/bin/npm-cli.js /usr/bin/npm
-  EOF
-end
-
-#yum_package "nodejs" do
-#  version "0.10.18-1.el6"
-#  action :install
-#end
-#
-#yum_package "npm" do
-#  action :install
-#end
-
-#### git ####
-yum_package "git" do
-  action :install
-end
-
 #### hostname, hosts ####
 template "hosts" do
   path "/etc/hosts"
@@ -78,11 +26,6 @@ ruby_block 'edit /etc/sysconfig/network' do
     rc.write_file
   end
 end
-
-
-# service "varnish"
-#  action [:start, :enable]
-# end
 
 
 
